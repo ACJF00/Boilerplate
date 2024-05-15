6c963c30c24e335ad47c7c9a074381db9c1b3acd
Charly M <chrlmztt@gmail.com>
2024-05-15 10:39:31 +0200

    webpack

---
+++ js/import.js
@@ -0,0 +1,6 @@
+import "./menu.js";
+import "./to-top.js";
+import "../node_modules/flowbite/dist/flowbite.min.js";
+import "./faq.js";
+import "./swipers.js";
+import "./fslightbox.js";
--- js/swipers.js
+++ js/swipers.js
@@ -1,9 +1,11 @@
-var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+if (document.querySelector(".swiper-container")) {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
